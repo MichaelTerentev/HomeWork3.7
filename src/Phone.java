2661d47000e3688dc0e464a0d7154148ad02cb56
MichaelTerentev <125468589+MichaelTerentev@users.noreply.github.com>
2023-05-12 23:00:31 +0500

    Update Phone.java

--- src/Phone.java
+++ src/Phone.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Phone {
+public class Phone implements Comparable<Phone> {
+	private final int price;
+	private final String model;
+	
+	public Phone(int price, String model) {
+		this.price = price;
+		this.model = model;
+	}
+	public int compareTo(Phone p) {
+		if(price > p.price) {
+			return 1;
+		}
+		else if(price < p.price) {
+			return -1;
+		}
+		else return 0;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public String getModel() {
+		return model;
+	}
+	@Override
+	public String toString() {
+		return model + ", " + price;
+	}
 }
