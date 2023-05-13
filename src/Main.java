bd71952136c1433535d71b5c55ff061c2e7eefb6
MichaelTerentev <125468589+MichaelTerentev@users.noreply.github.com>
2023-05-13 10:06:33 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        List<Phone> phones = new ArrayList<>();
-		Collections.addAll(phones, new Phone(3000, "Nokia"), new Phone(7500, "Samsung"), new Phone(3500, "Nokia"), new Phone(6000, "Samsung"), new Phone(10000, "IPhone"), new Phone(11000, "Samsung"));
-		//1
-		List<Phone> samsungPhones = phones.stream().filter(p -> p.getModel() == "Samsung").collect(Collectors.toList());
-		samsungPhones.forEach(p -> System.out.println(p.toString()));
-		//2
-		System.out.println("_______________\n");
-		phones.stream().sorted().forEach(p -> System.out.println(p.toString()));
+	List<Phone> phones = new ArrayList<>();
+        Collections.addAll(phones, new Phone(3000, "Nokia"), new Phone(7500, "Samsung"), new Phone(3500, "Nokia"), new Phone(6000, "Samsung"), new Phone(10000, "IPhone"), new Phone(11000, "Samsung"));
+	//1
+	List<Phone> samsungPhones = phones.stream().filter(p -> p.getModel() == "Samsung").collect(Collectors.toList());
+	samsungPhones.forEach(p -> System.out.println(p.toString()));
+	//2
+	System.out.println("_______________\n");
+	phones.stream().sorted().forEach(p -> System.out.println(p.toString()));
     }
 }
