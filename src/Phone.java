a6632b887275030936dacb6ea7f6a1fc52be8acc
MichaelTerentev <125468589+MichaelTerentev@users.noreply.github.com>
2023-05-13 10:07:53 +0500

    Update Phone.java

--- src/Phone.java
+++ src/Phone.java
@@ -1,22 +1,22 @@
 public class Phone implements Comparable<Phone> {
-	private final int price;
-	private final String model;
+    private final int price;
+    private final String model;
 	
-	public Phone(int price, String model) {
-		this.price = price;
-		this.model = model;
-	}
-	public int compareTo(Phone p) {
-		return price - p.price;
-	}
-	public int getPrice() {
-		return price;
-	}
-	public String getModel() {
-		return model;
-	}
-	@Override
-	public String toString() {
-		return model + ", " + price;
-	}
+    public Phone(int price, String model) {
+	this.price = price;
+	this.model = model;
+    }
+    public int compareTo(Phone p) {
+	return price - p.price;
+    }
+    public int getPrice() {
+	return price;
+    }
+    public String getModel() {
+	return model;
+    }
+    @Override
+    public String toString() {
+	return model + ", " + price;
+    }
 }
